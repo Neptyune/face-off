49b4f71c6dc82bb9d665f4631a50a96f65b575e0
Nick Michau <70205169+NOM1989@users.noreply.github.com>
2024-01-27 16:08:51 +0000

    mvp working

--- face_off/__init__.py
+++ face_off/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, Response
 import cv2
+from flask_socketio import SocketIO
+from deepface import DeepFace
+
 
 app = Flask(__name__)
+# app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
+
+camera = cv2.VideoCapture(0)
+if not camera.isOpened():
+    print("Unable to open camera")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")  # return index.html page
 
 
 def generate_frames():
-    camera = cv2.VideoCapture(4)
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
@@ -18,11 +31,6 @@ def generate_frames():
             )  # concat frame one by one and show result
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")  # return index.html page
-
-
 @app.route("/video")
 def video():
     return Response(
@@ -30,5 +38,24 @@ def video():
     )
 
 
+def update_emotions():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            print("Camera not working!")
+        else:
+            result = DeepFace.analyze(
+                frame, enforce_detection=False, actions=["emotion"]
+            )
+            socketio.emit("update_emotion", {"emotion": result[0]["emotion"]})
+
+        # socketio.sleep(1) # If enabled the number will lag behind the camera
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        socketio.start_background_task(update_emotions)
+        socketio.run(app, debug=False, use_reloader=False, log_output=False)
+    finally:
+        camera.release()
+        print("\nApplication exited and camera released")
